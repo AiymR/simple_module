c0866616c14d6abc456d51ee3255c6fd9725edee
Aiym <raikhanovaaiym@gmail.com>
2019-03-14 01:13:34 +0600

    user mode app

---
+++ application.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   	FILE *fp;//pointer to file
+	char word_ok[3];
+	char* ok="yes";
+
+	
+	//fp = fopen("/proc/authe", "r"); // reading mode to file
+	fp = fopen("/mnt/usb/test.txt", "r");
+
+	if (fp == NULL)
+   	{
+     		printf("Error while opening file.\n");
+      		exit(1);//if file doesn't exit or failed to open
+   	}	
+	
+	while(fscanf(fp, "%s",&word_ok) != EOF){ //reding one string from file till the end
+
+		if(strcmp(word_ok, ok) == 0)//returns 0 if string are equal, symbols
+		{
+			printf("USER AUTHENTICATION APPROVED\n");
+		}
+		else 
+		{
+		printf("USER AUTHENTICATION FAILED\n");
+		}
+	}
+	
+
+   fclose(fp); 
+  
+   return 0;
+}
